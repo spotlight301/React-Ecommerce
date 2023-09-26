fcb6d11c4fc878e5ddf894a2ed0a3fe431b4a206
plyn <plyn_rider@outlook.com>
2023-09-26 10:31:32 +0300

    Add files via upload

---
+++ HeaderSlide/index.js
@@ -0,0 +1,72 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="w-2/4 h-screen  grid grid-cols-3 grid-flow-dense gap-1 justify-center my-0 mx-auto p-1">
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 row-span-4 col-span-1 cursor-pointer rounded-3xl test">
+            <div className="layOut p-2 ">
+            <h3 className="text-center absolute top-10 left-20">Title</h3>
+            <p className="text-center absolute top-20 ">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 row-span-3 col-span-1 cursor-pointer rounded-3xl test">
+            <div className="layOut p-2 ">
+            <h3 className="text-center absolute top-10 left-20">Title</h3>
+            <p className="text-center absolute top-20 ">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 row-span-4 col-span-1 cursor-pointer rounded-3xl test">
+            <div className="layOut p-2">
+             <h3 className="text-center absolute top-10 left-20">Title</h3>
+                <p className="text-center absolute top-20 ">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 row-span-4 col-span-1 cursor-pointer rounded-3xl test">
+            <div className="layOut p-2">
+            <h3 className="text-center absolute top-10 left-20">Title</h3>
+            <p className="text-center absolute top-20 ">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 row-span-4 col-span-1 cursor-pointer rounded-3xl test">
+             <div className="layOut p-2">
+            <h3 className="text-center absolute top-10 left-20">Title</h3>
+            <p className="text-center absolute top-20 ">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 row-span-4 col-span-1 cursor-pointer rounded-3xl test">
+            <div className="layOut p-2">
+            <h3 className="text-center absolute top-10 left-20">Title</h3>
+            <p className="text-center absolute top-20 ">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 row-span-4 col-span-1 cursor-pointer rounded-3xl test">
+            <div className="layOut p-2">
+            <h3 className="text-center absolute top-10 left-20">Title</h3>
+            <p className="text-center absolute top-20 ">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 row-span-4 col-span-1 cursor-pointer rounded-3xl test">
+            <div className="layOut p-2">
+            <h3 className="text-center absolute top-10 left-20">Title</h3>
+            <p className="text-center absolute top-20 ">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 row-span-4 col-span-1 cursor-pointer rounded-3xl test">
+            <div className="layOut p-2">
+                <h3 className="text-center absolute top-10 left-20">Title</h3>
+                <p className="text-center absolute top-20 ">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+        
+
+    </div>
+    </>
+  )
+}
+
+export default App
