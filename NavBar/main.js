b042b7a110685293fc3a0d287b0b9b49b3da948d
plyn <plyn_rider@outlook.com>
2023-09-27 18:02:40 +0300

    Delete NavBar directory

--- NavBar/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("resp").onclick = function toggleMenu() {
-    const navToggle = document.getElementsByClassName("toggle");
-    for (let i = 0; i < navToggle.length; i++) {
-      navToggle.item(i).classList.toggle("hidden");
-    }
-  };
