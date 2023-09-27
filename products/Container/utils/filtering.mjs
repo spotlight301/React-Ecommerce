792b63e40129826d9c98302d95c8e8545d0c4442
plyn <plyn_rider@outlook.com>
2023-09-27 18:03:43 +0300

    Delete products directory

--- products/Container/utils/filtering.mjs
+++
@@ -1,9 +0,0 @@
-const compare= (txt,sub)=> txt.toLocaleLowerCase().includes(sub.toLocaleLowerCase())
-
-function filterData(data,filterTxt) {
-    return data.filter(
-        item => compare(item.name,filterTxt) || compare(item.brand,filterTxt)
-    )
-}
-
-export default filterData
--- products/README.md
+++
@@ -1,5 +0,0 @@
-> tto use json file inside JS use the following import
-```JSON
-import Data from './products.json' assert {type: 'json'};
-I used react-bootstrab library in this commponants و
-I couldn't include tailwind.css library in this project (rahma)
